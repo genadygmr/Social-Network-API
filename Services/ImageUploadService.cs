f2e7df2758662f6369aec2ed112792014a9a5d22
Genady <genadymgr@gmail.com>
2019-11-09 12:38:45 +0200

    Added image upload service

---
+++ Services/ImageUploadService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+
+namespace Social_Network_API.Services
+{
+    class ImageUploadService
+        {
+            private Cloudinary cloudinary;
+
+        public ImageUploadService()
+            {
+                Account account = new Account("doikwz68x", "942723343131761", "GUkuFeEWYyAhB8zWAxeYPwNKBfI");
+                cloudinary = new Cloudinary(account);
+            }
+        public ImageUploadResult uploadImage(string image)
+        {
+            var uploadParams = new ImageUploadParams()
+            {
+                File = new FileDescription(@image)
+            };
+
+            return cloudinary.Upload(uploadParams);
+        }
+
+    }    
+
+}
